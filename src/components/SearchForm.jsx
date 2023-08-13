d44dea97bcd8827fe74d2247a87a0f1c2fa5cbc3
Mohit Lahoti <engtechno25@gmail.com>
2023-08-14 01:09:09 +0530

    Install styled components in the index page and use Navlink and Global Theme

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { useSearhStr } from '../lib/useSearchStr';
+import CustomRadio from './customradio';
 const SerachForm = ({ onSearch }) => {
   const [searchOption, setSearchOption] = useState('shows');
   const [searchStr, setSearchStr] = useSearhStr();
-
   //1.mounts
   //2.rerender
   //2.5 Logic before next rerender.
@@ -32,28 +32,20 @@ const SerachForm = ({ onSearch }) => {
     <div>
       <form onSubmit={onSubmit}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
-        <label>
-          Shows
-          <input
-            type="radio"
-            name="search-option"
-            value="shows"
-            checked={searchOption == 'shows'}
-            onChange={onRadioChange}
-          />
-        </label>
-
-        <label>
-          Actors
-          <input
-            type="radio"
-            name="search-option"
-            value="actors"
-            checked={searchOption == 'actors'}
-            onChange={onRadioChange}
-          />
-        </label>
-
+        <CustomRadio
+          label="Shows"
+          name="search-option"
+          value="shows"
+          checked={searchOption == 'shows'}
+          onChange={onRadioChange}
+        />
+        <CustomRadio
+          label="Actors"
+          name="search-option"
+          value="shows"
+          checked={searchOption == 'shows'}
+          onChange={onRadioChange}
+        />
         <button type="submit">Search</button>
       </form>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,33 @@ import { searchForShows, searchForActors } from '../api/tvmaze';
 import SerachForm from '../components/SearchForm';
 import ShowsGrid from '../components/shows/ShowsGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import styled, { css, ThemeProvider } from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  color: 'palevioletred';
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `}
+
+  ${props =>
+    props.fontsize &&
+    css`
+      font-size: ${props.fontsize}px;
+    `}
+`;
 
 const Home = () => {
   const [filter, setFilter] = useState(null);
